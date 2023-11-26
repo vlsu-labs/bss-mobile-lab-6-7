c9ad9f325074ef7232b58bbd89cc8c052b172881
w1sp <semen2002bs@gmail.com>
2023-11-26 23:40:30 +0300

    some figures on screen

--- app/src/main/java/com/example/bss_mobile_lab_6_7/draw_utils/DrawView.kt
+++ app/src/main/java/com/example/bss_mobile_lab_6_7/draw_utils/DrawView.kt
@@ -2,36 +2,34 @@ package com.example.bss_mobile_lab_6_7.draw_utils
 
 import android.view.SurfaceView
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.view.SurfaceHolder
+import android.view.View
 
-class DrawView: SurfaceView, SurfaceHolder.Callback {
+class DrawView(context: Context): View(context) {
+    private val p: Paint = Paint()
+    private val rect: Rect = Rect()
 
-    private lateinit var drawThread: DrawThread
-
-    constructor(context: Context) : super(context) {
-        holder.addCallback(this)
-    }
-
-    override fun surfaceCreated(p0: SurfaceHolder) {
-        drawThread = DrawThread(holder)
-        drawThread.setRunning(true)
-        drawThread.start()
-    }
-
-    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-
-    }
-
-    override fun surfaceDestroyed(p0: SurfaceHolder) {
-        var retry: Boolean = true
-        drawThread.setRunning(false)
-        while (retry) {
-            try {
-                drawThread.join()
-                retry = false
-            } catch (e: InterruptedException) {
-
-            }
+    override fun onDraw(canvas: Canvas?) {
+        if (canvas == null) {
+            return
         }
+
+        canvas.drawARGB(80, 102, 204, 255)
+
+        p.color = Color.RED
+        p.strokeWidth = 10F
+
+        canvas.drawPoint(50F, 50F, p)
+        canvas.drawLine(100F, 100F, 500F, 50F, p)
+        canvas.drawCircle(100F, 200F, 50F, p)
+        canvas.drawRect(200F, 150F, 400F, 200F, p)
+
+        rect.set(250, 300, 350, 500)
+        canvas.drawRect(rect, p)
+
     }
 }
