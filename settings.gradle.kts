bf223ba96c563f956a76f7a6fd757700fb61b397
w1sp <semen2002bs@gmail.com>
2023-11-26 22:49:52 +0300

    commit all

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "bss-mobile-lab-6-7"
+include(":app")
+ 
